acce1e01a1e82aff1d6b15e7668623e5ed5476cc
EstebanDaniel <estebandanielcardozo65@gmail.com>
2023-01-11 17:01:39 -0300

    Se realizaron modificaciones en CSS academic, y se agrego async await para envio de formulario

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { valida } from "./validacion.js";
 
+const $form = document.getElementById('form');
 export const submitBtn = document.getElementById('submitBtn');
 const inputs = document.querySelectorAll("input");
 const textareas = document.querySelectorAll("textarea");
@@ -18,7 +19,24 @@ textareas.forEach((textarea) => {
 });
 
 
- 
+ $form.addEventListener('submit',handleSubmit);
+
+
+async function handleSubmit(event){
+  event.preventDefault();
+  const form = new FormData(this);
+ const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers:{
+      'Accept': 'application/json'
+    }
+  })
+  if(response.ok){
+    this.reset();
+    alert("formulario enviado!!")
+  }
+ }
 
 
 
